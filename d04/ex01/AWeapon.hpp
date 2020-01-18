8036f62c13ae640488e189852b9c14cc57903250
Sanghyun Ko <movingfinger7@gmail.com>
2020-01-18 15:49:08 -0800

    update d04/ex01/AWeapon.hpp

--- d04/ex01/AWeapon.hpp
+++ d04/ex01/AWeapon.hpp
@@ -1,5 +1,26 @@
 #ifndef AWEAPON_H
 #define AWEAPON_H
 
+#include <iostream>
+#include <string>
+
+class AWeapon
+{
+	public:
+		AWeapon(std::string const & name, int apcost, int damage);
+		~AWeapon(void);
+
+		std::string		getName() const;
+		int 			getAPCost() const;
+		int				getDamage() const;
+
+		void			attack() const = 0;
+	
+	private:
+		std::string		_name;
+		int				_nodp;
+		int				_ap;
+};
+
 #endif
 
